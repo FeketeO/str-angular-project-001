05d95c4acf2b0dc0b0835245bb2c62f69bd645ed
Várdai Enikő <vardai.eniko@gmail.com>
2021-02-08 17:50:43 +0100

    lapozo kész

--- src/app/common/data-editor/data-editor.component.ts
+++ src/app/common/data-editor/data-editor.component.ts
@@ -16,6 +16,11 @@ export class DataEditorComponent implements OnInit {
   filterKey: string = 'name';
   filterKeys: string[] = Object.keys(new Product());
   page: number = 1;
+  isActive1: boolean = true;
+  isActive2: boolean = false;
+  isActive3: boolean = false;
+  isActive4: boolean = false;
+  isActive5: boolean = false;
   
   constructor(
     private productService: ProductService,
@@ -40,7 +45,42 @@ export class DataEditorComponent implements OnInit {
   }
 
   active(pageIn: number) {
-    this.page = pageIn;   
-    
+    this.page = pageIn;  
+    if(pageIn==1){
+
+      this.isActive1 = true;
+      this.isActive2 = false;
+      this.isActive3 = false;
+      this.isActive4 = false;
+      this.isActive5 = false;
+    } else if (pageIn==2){
+
+      this.isActive1 = false;
+      this.isActive2 = true;
+      this.isActive3 = false;
+      this.isActive4 = false;
+      this.isActive5 = false;
+    } else if(pageIn==3){
+
+      this.isActive1 = false;
+      this.isActive2 = false;
+      this.isActive3 = true;
+      this.isActive4 = false;
+      this.isActive5 = false;
+    } else if(pageIn==4){
+
+      this.isActive1 = false;
+      this.isActive2 = false;
+      this.isActive3 = false;
+      this.isActive4 = true;
+      this.isActive5 = false;
+    } else if(pageIn==5){
+
+      this.isActive1 = false;
+      this.isActive2 = false;
+      this.isActive3 = false;
+      this.isActive4 = false;
+      this.isActive5 = true;
+    } 
     }
 }
